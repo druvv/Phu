2a1892abab2d407cb250121aa8359cd681414fb3
Dhruv Sringari <dhruv@havanote.com>
2018-10-28 19:10:46 -0400

    Program ready for initial testing

---
+++ PlatformIO/src/Phu.ino
@@ -0,0 +1,229 @@
+#include "Arduino.h"
+#include "NewPing.h"
+
+const int motorRightPWM = 11;
+const int motorRightA = 5;
+const int motorRightB = 4;
+
+const int motorLeftPWM = 10;
+const int motorLeftA = 3;
+const int motorLeftB = 2;
+
+const int frontTrig = 7;
+const int frontEcho = 6;
+
+const int leftTrig = 9;
+const int leftEcho = 8;
+
+const int rightTrig = 13;
+const int rightEcho = 12;
+
+// -- MARK: PROGRAM CONFIGURATION
+const int MAX_DISTANCE = 200; // cm
+const int WALL_DETECT_THRESHOLD = 10; // 10 cm
+const int TURN_TIME = 1000; // ms
+const int WALL_AVOIDANCE_TIME = 500; // ms
+bool defaultTurnIsLeft = true;
+
+enum TurnDirection {counterclockwise, clockwise, around};
+enum Facing {left, right, forwards};
+Facing robotDirection = forwards;
+
+NewPing frontSonar(frontTrig, frontEcho, MAX_DISTANCE);
+NewPing leftSonar(leftTrig, leftEcho, MAX_DISTANCE);
+NewPing rightSonar(rightTrig, rightEcho, MAX_DISTANCE);
+
+void setup() {
+  Serial.begin(9600);
+
+  while (!Serial) {} // Wait for serial to connect
+
+  Serial.println("Awake!");
+
+  pinMode(motorLeftPWM, OUTPUT);
+  pinMode(motorLeftA, OUTPUT);
+  pinMode(motorLeftB, OUTPUT);
+
+  pinMode(motorRightPWM, OUTPUT);
+  pinMode(motorRightA, OUTPUT);
+  pinMode(motorRightB, OUTPUT);
+
+  digitalWrite(motorLeftA, LOW);
+  digitalWrite(motorLeftB, HIGH);
+  digitalWrite(motorRightA, LOW);
+  digitalWrite(motorRightB, HIGH);
+}
+
+// - MARK: MOTOR SPEEDS
+
+// -255 to 255
+void setLeft(int s) {
+  if (s > 0) {
+    digitalWrite(motorLeftA, LOW);
+    digitalWrite(motorLeftB, HIGH);
+  } else if (s < 0) {
+    digitalWrite(motorLeftA, HIGH);
+    digitalWrite(motorLeftB, LOW);
+  } else {
+    digitalWrite(motorLeftA, LOW);
+    digitalWrite(motorLeftB, LOW);
+  }
+
+  s = abs(s);
+  //Serial.print("Left: " + String(s));
+
+  analogWrite(motorLeftPWM, s);
+}
+
+void setRight(int s) {
+  if (s > 0) {
+    digitalWrite(motorRightA, LOW);
+    digitalWrite(motorRightB, HIGH);
+  } else if (s < 0)  {
+    digitalWrite(motorRightA, HIGH);
+    digitalWrite(motorRightB, LOW);
+  } else {
+    digitalWrite(motorRightA, LOW);
+    digitalWrite(motorRightB, LOW);
+  }
+
+  s = abs(s);
+  //Serial.print("Right: " + String(s));
+
+  analogWrite(motorRightPWM, s);
+}
+
+// - MARK: TURNING
+
+void rotateRobotDirection(TurnDirection direction) {
+  switch (robotDirection) {
+  case left:
+    if (direction == clockwise) {
+      robotDirection = forwards;
+    }
+
+    if (direction == around) {
+      robotDirection = right;
+    }
+
+    break;
+  case right:
+    if (direction == counterclockwise) {
+      robotDirection = forwards;
+    }
+
+    if (direction == around) {
+      robotDirection = left;
+    }
+
+    break;
+  case forwards:
+    if (direction == counterclockwise) {
+      robotDirection = left;
+    }
+
+    if (direction == clockwise) {
+      robotDirection = right;
+    }
+
+    break;
+  }
+
+  if (robotDirection == forwards) {
+    Serial.println("DIR: F");
+  } else if (robotDirection == left) {
+    Serial.println("DIR: L");
+  } else {
+    Serial.println("DIR: R");
+  }
+}
+
+void turn(TurnDirection direction) {
+  switch (direction) {
+  case counterclockwise:
+    setLeft(-250);
+    setRight(250);
+    delay(TURN_TIME);
+    break;
+  case clockwise:
+    setLeft(250);
+    setRight(-250);
+    robotDirection = right;
+    delay(TURN_TIME);
+    break;
+  case around:
+    setLeft(-250);
+    setRight(250);
+    if (robotDirection == left) {
+      robotDirection = right;
+    } else {
+      robotDirection = left;
+    }
+    delay(TURN_TIME * 2);
+    break;
+  }
+
+  rotateRobotDirection(direction);
+}
+
+void moveForwards() {
+  setLeft(250);
+  setRight(250);
+}
+
+// - MARK: MAIN LOOP
+
+void loop() {
+  int frontDistance = frontSonar.ping_cm();
+  int leftDistance = leftSonar.ping_cm();
+  int rightDistance = rightSonar.ping_cm();
+
+  // If we are facing forwards, move forward until we detect a wall.
+  if (robotDirection == forwards && frontDistance <= WALL_DETECT_THRESHOLD) {
+    int difference = leftDistance - rightDistance;
+    // If the left and right walls are about equally apart turn in the default direction.
+    if (abs(difference) < 10) {
+      if (defaultTurnIsLeft) {
+        turn(counterclockwise);
+      } else {
+        turn(clockwise);
+      }
+    // If the left wall is closer than the right wall, move right.
+    } else if (difference < 0) {
+      turn(clockwise);
+    // If the right wall us closer than the left wall, move left.
+    } else {
+      turn(counterclockwise);
+    }
+  // If we are facing left, move until we don't detect a right wall.
+  } else if (robotDirection == left) {
+    // If there are walls on all three sides, turn around.
+    if (leftDistance <= WALL_DETECT_THRESHOLD && rightDistance <= WALL_DETECT_THRESHOLD && frontDistance <= WALL_DETECT_THRESHOLD) {
+      turn(around);
+    } else if (rightDistance > WALL_DETECT_THRESHOLD) {
+      // Move a little to center ourselves, then go forwards.
+      moveForwards();
+      delay(WALL_AVOIDANCE_TIME);
+      turn(clockwise);
+    } else {
+      moveForwards();
+    }
+   // If we are facing right, move until we don't detect a left wall. 
+  } else if (robotDirection == right) {
+    // If there are walls on all three sides, turn around.
+    if (leftDistance <= WALL_DETECT_THRESHOLD && rightDistance <= WALL_DETECT_THRESHOLD && frontDistance <= WALL_DETECT_THRESHOLD) {
+      turn(around);
+    } else if (leftDistance > WALL_DETECT_THRESHOLD) {
+      // Move a little to center ourselves, then go forwards.
+      moveForwards();
+      delay(WALL_AVOIDANCE_TIME);
+      turn(counterclockwise);
+    } else {
+      moveForwards();
+    }
+  } else {
+    moveForwards();
+  }
+} 
+
+
